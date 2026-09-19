eada7d4b5b43d856b48a68a18dba69dd6159af5a
Stefan <stefan.migo@gmail.com>
2026-09-19 18:29:39 -0300

    refactor(cli): drop the template retrospective wrapper

--- cli/src/template/scripts/generate-retrospective.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# scripts/generate-retrospective.sh
-# Called by: rapso close --retrospective
-# Generates .rapsodia-code/retrospectives/YYYY-MM-DD-sessionId.md
-rapso close --retrospective "$@"
